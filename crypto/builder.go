a8c3d604dd44fd75a66e39423decb36d941b458e
utkn <39ph4f87a@gmail.com>
2022-12-13 14:45:31 +0100

    LRB comments

--- crypto/builder.go
+++ crypto/builder.go
@@ -8,6 +8,7 @@ import (
 	"github.com/tuneinsight/lattigo/v4/ring"
 )
 
+// term represents A*b
 type term struct {
 	A           *fastmath.IntMatrix
 	b           *fastmath.IntVec
@@ -15,18 +16,20 @@ type term struct {
 	depVecIndex int
 }
 
+// LinearEquation represents a linear equation of form lhs = \sum_{i=1}^k A_i * b_i
 type LinearEquation struct {
 	lhs       *fastmath.IntVec
 	rhs       []term
-	m         int
-	dependent bool
+	m         int  // # rows of the equation, i.e. size of lhs.
+	dependent bool // denotes whether the equation contains a dependent term.
 }
 
+// NewLinearEquation constructs an empty equation from the given lhs.
 func NewLinearEquation(lhs *fastmath.IntVec, cols int) *LinearEquation {
 	return &LinearEquation{lhs, []term{}, lhs.Size(), false}
 }
 
-// AppendTerm adds a new term to this equation.
+// AppendTerm adds a new term A*b to this equation.
 func (eqn *LinearEquation) AppendTerm(A *fastmath.IntMatrix, b *fastmath.IntVec) *LinearEquation {
 	if A.Rows() != eqn.m || A.Cols() != b.Size() {
 		panic("cannot append term with invalid size")
@@ -35,21 +38,24 @@ func (eqn *LinearEquation) AppendTerm(A *fastmath.IntMatrix, b *fastmath.IntVec)
 	return eqn
 }
 
+// AppendVecTerm appends a new vector term Id*b to this equation.
 func (eqn *LinearEquation) AppendVecTerm(b *fastmath.IntVec, baseRing *ring.Ring) *LinearEquation {
 	id := fastmath.NewIdIntMatrix(b.Size(), baseRing)
 	eqn.rhs = append(eqn.rhs, term{id, b, false, 0})
 	return eqn
 }
 
-// AddDependentTerm adds a dependent term to this equation with the associated vector being defined
-// in another equation, indicated by its position in the system by `vecIndex`.
+// AddDependentTerm adds a dependent term to this equation with the associated vector defined in another equation,
+// indicated by its position in the system by `vecIndex`.
 func (eqn *LinearEquation) AppendDependentTerm(A *fastmath.IntMatrix, vecIndex int) *LinearEquation {
 	eqn.rhs = append(eqn.rhs, term{A, nil, true, vecIndex})
 	eqn.dependent = true
 	return eqn
 }
 
-// Note: The referenced vector must have the correct size!!
+// AppendDependentVecTerm adds a dependent vector term to this equation with the vecrtor defined in another equation,
+// indicated by its position in the system by `vecIndex`.
+// Note: The referenced vector must have the correct size (i.e., m) !!
 func (eqn *LinearEquation) AppendDependentVecTerm(vecIndex int, baseRing *ring.Ring) *LinearEquation {
 	id := fastmath.NewIdIntMatrix(eqn.m, baseRing)
 	eqn.rhs = append(eqn.rhs, term{id, nil, true, vecIndex})
@@ -59,6 +65,7 @@ func (eqn *LinearEquation) AppendDependentVecTerm(vecIndex int, baseRing *ring.R
 
 // AddDependency adds a dependency to this equation.
 func (eqn *LinearEquation) AddDependency(termEqnIndex, vecSystemIndex int) *LinearEquation {
+	// Set the appropriate term as dependent.
 	eqn.rhs[termEqnIndex].depVecIndex = vecSystemIndex
 	eqn.rhs[termEqnIndex].dependent = true
 	eqn.rhs[termEqnIndex].b = nil
@@ -66,14 +73,17 @@ func (eqn *LinearEquation) AddDependency(termEqnIndex, vecSystemIndex int) *Line
 	return eqn
 }
 
+// IsDependent returns true iff this equation contains a dependent term.
 func (eqn *LinearEquation) IsDependent() bool {
 	return eqn.dependent
 }
 
+// GetTerms returns the list of terms in this equation.
 func (eqn *LinearEquation) GetTerms() []term {
 	return eqn.rhs
 }
 
+// GetDependentTerms returns the list of dependent terms in this equation.
 func (eqn *LinearEquation) GetDependentTerms() []term {
 	dependentTerms := []term{}
 	for _, t := range eqn.GetTerms() {
@@ -84,6 +94,7 @@ func (eqn *LinearEquation) GetDependentTerms() []term {
 	return dependentTerms
 }
 
+// GetIndependentTerms returns the list of independent terms in this equation.
 func (eqn *LinearEquation) GetIndependentTerms() []term {
 	independentTerms := []term{}
 	for _, t := range eqn.GetTerms() {
@@ -109,6 +120,8 @@ func (eqn *LinearEquation) Linearize() LinearRelation {
 	return linRel
 }
 
+// String returns a string representation of this equation.
+// TODO: fix
 func (eqn *LinearEquation) String() string {
 	strs := make([]string, 0)
 	for i := 0; ; i++ {
@@ -168,6 +181,8 @@ func (lrb *LinearRelationBuilder) AppendEqn(eqn *LinearEquation) *LinearRelation
 	return lrb
 }
 
+// getZeroPad constructs a zero-padding for j-th term of i-th equation in the given system of equations by dynamically
+// selecting an appropriate column length.
 func getZeroPad(i, j int, eqns []*LinearEquation, baseRing *ring.Ring) *fastmath.IntMatrix {
 	// Try to find an appropriate matrix among the equations to determine the zero padding column size.
 	var targetEqn *LinearEquation
@@ -197,17 +212,22 @@ func (lrb *LinearRelationBuilder) Build(baseRing *ring.Ring) LinearRelation {
 	linRel := lrb.eqns[0].Linearize()
 	for i, eqn := range lrb.eqns[1:] {
 		if eqn.IsDependent() {
+			// For a dependent equation we want to find a B, y s.t. (A || 0, B) (s, y) = (u, lhs) will yield
+			// the correct linear relation.
 			prevEqnNumIndepTerms := 0
 			for j := i; j >= 0; j-- {
 				prevEqnNumIndepTerms += len(lrb.eqns[j].GetIndependentTerms())
 			}
+			// First, emplace the dependent term matrices into the correct position.
 			preB := make([]*fastmath.IntMatrix, prevEqnNumIndepTerms)
 			for _, t := range eqn.GetDependentTerms() {
 				preB[t.depVecIndex] = t.A
 			}
+			// Then, append the independent term matrices.
 			for _, t := range eqn.GetIndependentTerms() {
 				preB = append(preB, t.A)
 			}
+			// Iteratively build up the new row in the matrix.
 			var B *fastmath.IntMatrix
 			if preB[0] == nil {
 				B = getZeroPad(i+1, 0, lrb.eqns, baseRing)
@@ -221,6 +241,7 @@ func (lrb *LinearRelationBuilder) Build(baseRing *ring.Ring) LinearRelation {
 					B.ExtendCols(m)
 				}
 			}
+			// Build the y, the concetanation of term vectors.
 			y := eqn.GetIndependentTerms()[0].b
 			for _, t := range eqn.GetIndependentTerms()[1:] {
 				y.Append(t.b)
@@ -233,6 +254,8 @@ func (lrb *LinearRelationBuilder) Build(baseRing *ring.Ring) LinearRelation {
 	return linRel
 }
 
+// String returns a string representation this LRB, i.e., a listing of all the appended
+// equations up to now.
 func (lrb *LinearRelationBuilder) String() string {
 	strs := make([]string, 0, len(lrb.eqns))
 	for i, eqn := range lrb.eqns {
