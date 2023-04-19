ca454b573c238d3eade3972e200b5c0ad5781458
utkn <39ph4f87a@gmail.com>
2023-04-19 16:12:05 +0200

    experiments script

---
+++ experiments.sh
@@ -0,0 +1,21 @@
+mkdir tbl1 tbl2 tbl3 tbl4 tbl5 tbl6
+
+go run main.go --n 10 --threads 1 --rel keygen > tbl1/keygen.txt
+go run main.go --n 10 --threads 1 --rel collective_dec > tbl2/collective_dec.txt
+go run main.go --n 10 --threads 1 --rel keyswitch > tbl2/keyswitch.txt
+go run main.go --n 10 --threads 1 --rel bootstrapping > tbl2/bootstrapping.txt
+go run main.go --n 10 --threads 1 --rel relinkeygen > tbl2/relinkeygen.txt
+go run main.go --n 10 --threads 1 --rel aggr_keygen > tbl3/aggr_keygen.txt
+go run main.go --n 10 --threads 1 --rlwe 11 > tbl4/keygen11.txt
+go run main.go --n 10 --threads 1 --rlwe 12 > tbl4/keygen12.txt
+go run main.go --n 10 --threads 1 --rlwe 13 > tbl4/keygen13.txt
+go run main.go --n 10 --threads 1 --rlwe 14 > tbl4/keygen14.txt
+go run main.go --n 10 --threads 1 --rlwe 15 > tbl4/keygen15.txt
+go run main.go --n 10 --threads 1 --levels 1 > tbl5/keygen1.txt
+go run main.go --n 10 --threads 1 --levels 2 > tbl5/keygen2.txt
+go run main.go --n 10 --threads 1 --levels 3 > tbl5/keygen3.txt
+go run main.go --n 10 --threads 1 --commt 7 --lambda 10 --kappa 9 --delta1 25 > tbl6/keygen7.txt
+go run main.go --n 10 --threads 1 --commt 10 --lambda 1 --kappa 2 --delta1 29 > tbl6/keygen10.txt
+go run main.go --n 10 --threads 1 --commt 13 --lambda 1 --kappa 1 --delta1 32 > tbl6/keygen13.txt
+
+
